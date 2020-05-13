9829662287c091e9eb94f4d4bf7a1b9e862c076a
PapaSmurf <PapaSmurf323@gmail.com>
2020-05-13 14:06:43 -0400

    airline theme change

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ cd Vim/.vim/bundle/
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
 git clone https://github.com/powerline/fonts.git
 cd -
 echo Finish Cloning Vim Plugins
